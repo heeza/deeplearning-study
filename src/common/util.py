d9e09d9d4757d61bf5315dc864e384fab8216f74
heeza <heeza@ymail.com>
2025-06-04 17:43:51 +0900

    DATA SET 추가 건

--- src/common/util.py
+++ src/common/util.py
@@ -6,31 +6,35 @@ def preprocess(text):
     words = text.split(' ')
 
     word_to_id = {}
-    id_to_word = {}
-
     for word in words:
         if word not in word_to_id:
-            new_id = len(word_to_id)
+            new_id =  len(word_to_id)
             word_to_id[word] = new_id
-            id_to_word[new_id] = word
-
-    corpus = np.array([word_to_id[w] for w in words])
-
+            
+    id_to_word = {id_: word for word, id_ in word_to_id.items()}
+    
+    corpus = np.array([word_to_id[word] for word in words])
     return corpus, word_to_id, id_to_word
 
 def create_co_matrix(corpus, vocab_size, window_size=1):
+    '''동시발생 행렬 생성
+    :param corpus: 말뭉치(단어 ID 목록)
+    :param vocab_size: 단어 수
+    :param window_size: 윈도우 크기(윈도우 크기가 1이면 타깃 단어 좌우 한 단어씩이 맥락에 포함)
+    :return: 동시발생 행렬
+    '''
     corpus_size = len(corpus)
     co_matrix = np.zeros((vocab_size, vocab_size), dtype=np.int32)
-
+    
     for idx, word_id in enumerate(corpus):
         for i in range(1, window_size + 1):
-            left_idx = idx - i
-            right_idx = idx + i
-            
+            left_idx = idx - i  # left window_size
+            right_idx = idx + i  # right window_size
+
             if left_idx >= 0:
                 left_word_id = corpus[left_idx]
                 co_matrix[word_id, left_word_id] += 1
-                
+
             if right_idx < corpus_size:
                 right_word_id = corpus[right_idx]
                 co_matrix[word_id, right_word_id] += 1
@@ -38,31 +42,70 @@ def create_co_matrix(corpus, vocab_size, window_size=1):
     return co_matrix
 
 def cos_similarity(x, y, eps=1e-8):
-    nx = x / np.sqrt(np.sum(x**2) + eps)
-    ny = y / np.sqrt(np.sum(y**2) + eps)
+    '''코사인 유사도 산출
+    :param x: 벡터
+    :param y: 벡터
+    :param eps: '0으로 나누기'를 방지하기 위한 작은 값
+    :return:
+    '''
+    nx = x / np.sqrt(np.sum(x**2) + eps)  # x의 정규화
+    ny = y / np.sqrt(np.sum(y**2) + eps)  # y의 정규화
     return np.dot(nx, ny)
 
 def most_similar(query, word_to_id, id_to_word, word_matrix, top=5):
+    '''유사 단어 검색
+    :param query: 쿼리(텍스트)
+    :param word_to_id: 단어에서 단어 ID로 변환하는 딕셔너리
+    :param id_to_word: 단어 ID에서 단어로 변환하는 딕셔너리
+    :param word_matrix: 단어 벡터를 정리한 행렬. 각 행에 해당 단어 벡터가 저장되어 있다고 가정한다.
+    :param top: 상위 몇 개까지 출력할 지 지정
+    '''
+    # 1) 검색어를 꺼낸다.
     if query not in word_to_id:
-        print('%s를 찾을 수 없습니다.')
-        return
+        print(f'{query}(을)를 찾을 수 없습니다.')
+        return 
     
-    print('\n[query] ' + query)
+    print(f'\n[query] {query}')
     query_id = word_to_id[query]
     query_vec = word_matrix[query_id]
-
+    
+    # 2) 코사인 유사도 계산
     vocab_size = len(id_to_word)
     similarity = np.zeros(vocab_size)
-    
     for i in range(vocab_size):
         similarity[i] = cos_similarity(word_matrix[i], query_vec)
-    
+        
+    # 3) 코사인 유사도를 기준으로 내림차순으로 출력
     count = 0
     for i in (-1 * similarity).argsort():
         if id_to_word[i] == query:
             continue
-        print(' %s: %s' % (id_to_word[i], similarity[i]))
-
-        count += 1
+        print(f' {id_to_word[i]}: {similarity[i]}')
+        
+        count +=1
         if count >= top:
             return
+
+# common/util.py
+def ppmi(C, verbose=False, eps=1e-8):
+    '''PPMI(점별 상호정보량) 생성
+    :param C: 동시발생 행렬
+    :param verbose: 진행 상황을 출력할지 여부
+    :return: ppmi
+    '''
+    M = np.zeros_like(C, dtype=np.float32)
+    N = np.sum(C)  # num of corpus
+    S = np.sum(C, axis=0)  # 각 단어의 출현 횟수
+    total = C.shape[0] * C.shape[1]
+    cnt = 0
+    
+    for i in range(C.shape[0]):
+        for j in range(C.shape[1]):
+            pmi = np.log2(C[i, j] * N / (S[i]*S[j]) + eps)
+            M[i, j] = max(0, pmi)
+            
+            if verbose:
+                cnt += 1
+                if cnt % (total//100) == 0:
+                    print(f'{(100*cnt/total):.2f} 완료')
+    return M
